ee6142be481049084490557c28ae3ec55b7fc7e9
Jackson Coxson <jkcoxson@gmail.com>
2022-08-14 18:38:07 -0600

    Add globals

---
+++ moabdx/globals.py
@@ -0,0 +1,27 @@
+# Jackson Coxson
+
+_api_key = ""
+_dx_url = "https://moabdx.com/api/v1/"
+
+
+def login(key: str):
+    """
+    Logs in to the API to provide data that is not publically available
+    :param key: The API key to use
+    :return: None
+    """
+
+    global _api_key
+    _api_key = key
+
+
+def set_dx_url(url: str):
+    """
+    Sets the URL for the MoabDX API
+    Only useful for testing
+    :param url: The URL to use
+    :return: None
+    """
+
+    global _dx_url
+    _dx_url = url
