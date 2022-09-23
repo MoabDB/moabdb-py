247b178a36afecf7906b3d358cf3d29853322a2d
Jackson Coxson <jkcoxson@gmail.com>
2022-09-23 16:58:31 -0600

    Append the encoded string

--- moabdx/protocol.py
+++ moabdx/protocol.py
@@ -5,7 +5,7 @@ import base64
 
 
 class Reqres:
-    def __init__(self, version: int, op: int, body: List, auth: str = None):
+    def __init__(self, version: int, op: int, body, auth: str = None):
         """
         Initializes a request/response packet
 
@@ -24,6 +24,33 @@ class Reqres:
         else:
             buffer.append(1)
             buffer.extend(self.auth.encode())
-        buffer.extend(self.body)
-        pls = base64.b64encode(buffer)
+        buffer += self.body.serialize()
+
+        buf_len = len(buffer).to_bytes(4, 'little')
+        buf_len = bytearray(buf_len)
+        buf_len += buffer
+
+        pls = base64.b64encode(buf_len)
         return pls.decode('ascii')
+
+
+class DataRequest:
+    def __init__(self, data_type: int, ticker: str, intraday: bool, start: int, end: int):
+        self.data_type = data_type
+        self.ticker = ticker
+        self.intraday = intraday
+        self.start = start
+        self.end = end
+
+    def serialize(self) -> bytearray:
+        buffer = bytearray([])
+        buffer.append(self.data_type)
+        buffer.append(len(self.ticker))
+        buffer.extend(self.ticker.encode())
+        if (self.intraday):
+            buffer.append(1)
+        else:
+            buffer.append(0)
+        buffer += self.start.to_bytes(4, 'little')
+        buffer += self.end.to_bytes(4, 'little')
+        return buffer
--- tests/test.py
+++ tests/test.py
@@ -2,7 +2,12 @@
 
 import moabdx as mdx
 from moabdx import protocol
+import requests
 
-r = protocol.Reqres(1, 4, [1, 2, 3, 4], auth="yarhar")
-b6 = r.serialize()
-print(b6)
+r = protocol.Reqres(1, 4, protocol.DataRequest(
+    1, "TSLA", False, 0, 12345)).serialize()
+
+print(r)
+
+res = requests.get(mdx.globals._dx_url + 'request/', headers={'x-req': r})
+print(res.text)
