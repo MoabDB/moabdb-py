6a7ef0f4f746b21fe1deb4dd590ce0179ea499bf
Jackson Coxson <jkcoxson@gmail.com>
2022-12-02 18:04:10 -0700

    Fix typos and missing code

--- moabdb/lib.py
+++ moabdb/lib.py
@@ -7,18 +7,31 @@ from . import protocol_pb2
 from . import convert_seconds, get_seconds_w_freq, time_window
 
 from base64 import b64encode, b64decode
+import pandas as pd
 
 
 def hello():
     print("Welcome to Moab, where all data is exchanged!!")
 
 
-def check_version() -> bool:
+def _check_version() -> bool:
     """
     Checks the server's version, compairing the current version
     """
     res = requests.get(globals._dx_url + 'client_version/')
     return (res.text == __version__)
+
+
+def _check_access() -> bool:
+    """
+    Checks if the user has set the global credentials.
+    This function is just a local sanity check, the server will
+    check the credentials again.
+    """
+    if globals._api_key == "":
+        return False
+    elif globals._api_username == "":
+        return False
 
 
 def send_request(request: protocol_pb2.Request) -> protocol_pb2.Response:
@@ -36,7 +49,7 @@ def send_request(request: protocol_pb2.Request) -> protocol_pb2.Response:
     return res
 
 
-def server_req(ticker, start, end, datatype):
+def _server_req(ticker, start, end, datatype):
     # Request data from moabdb server
     req = protocol_pb2.Request()
     req.symbol = ticker
@@ -50,64 +63,67 @@ def server_req(ticker, start, end, datatype):
         # Place data into a dataframe
         pq_file = io.BytesIO(res.data)
         df = pd.read_parquet(pq_file)
-        return(df)
+        return (df)
     else:
         print(res.code)
         print(res.message)
 
 
-def get_equity(tickers, sample="5d", 
-            start=None, end=None,
-            intraday=False, api_key=None):
-        """
-        get_equity paramaters:
-            tickers: str OR list of strings
-                  Ex: "NVDA" or ["NVDA","AMD"]
-            sample: str
-                Sample length, required if "start" or "end" is missing
-                Enter as number then frequency string (D, W, M, Y)
-                  Ex: "30d", "3m", "5y", etc.
-            start: str
-                Beginning date of sample, required if "end" or "sample" is missing
-                  Ex: '2020-01-01'
-            end: str
-                Ending date of sample, required if "start" or "sample" is missing
-                  Ex: '2022-05-01''
-            intraday: bool
-                True to return intraday data
-                Default is 'False' to return end-of-day data
-                See moabdb.com to look at subscriptions for intraday access
-            api_key: str
-                api_key required for intraday data
-        """
+def get_equity(tickers, sample="5d",
+               start=None, end=None,
+               intraday=False):
+    """
+    get_equity paramaters:
+        tickers: str OR list of strings
+              Ex: "NVDA" or ["NVDA","AMD"]
+        sample: str
+            Sample length, required if "start" or "end" is missing
+            Enter as number then frequency string (D, W, M, Y)
+              Ex: "30d", "3m", "5y", etc.
+        start: str
+            Beginning date of sample, required if "end" or "sample" is missing
+              Ex: '2020-01-01'
+        end: str
+            Ending date of sample, required if "start" or "sample" is missing
+              Ex: '2022-05-01''
+        intraday: bool
+            True to return intraday data
+            Default is 'False' to return end-of-day data
+            See moabdb.com to look at subscriptions for intraday access
+        api_key: str
+            api_key required for intraday data
+    """
 
     # Check intraday authorization
     if (intraday == True):
-        is_authorized = _check_access(api_key)
+        is_authorized = _check_access()
         equity_freq = "intraday_stocks"
         if is_authorized == 0:
+            # TODO: Add error code
             print("Error: Subscription required to access intraday data")
             intraday = False
             equity_freq = "daily_stocks"
 
-    # String time to integer timne
+    # String time to integer time
     start_tm, end_tm = _request_window(sample_len, start, end)
 
-    # Single ticker request 
+    # Single ticker request
     if isinstance(tickers, str):
-        return_db = _server_req(str.upper(tickers), start_tm, end_tm, equity_freq)
+        return_db = _server_req(
+            str.upper(tickers), start_tm, end_tm, equity_freq)
 
     # List of tickers request
-    else if isinstance(tickers, list):
+    elif isinstance(tickers, list):
         compile_tickers = []
         for tic in tickers:
-            compile_tickers.append(_server_req(str.upper(tic), start_tm, end_tm, equity_freq))
+            compile_tickers.append(_server_req(
+                str.upper(tic), start_tm, end_tm, equity_freq))
         return_db = pd.concat(compile_tickers)
-        return_db = return_db.set_index(['Ticker','Date']).unstack(0)
+        return_db = return_db.set_index(['Ticker', 'Date']).unstack(0)
 
     # Unknown ticker request
     else:
+        # TODO: Add error code
         print("Error accessing tickers: Enter ticker or list of tickers")
 
-    return(return_db)
-
+    return (return_db)
