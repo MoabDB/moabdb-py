97b6c871b422bf1158996a19ccc8167be1ab62d3
Brian Roseman <brianroseman@gmail.com>
2022-12-07 15:40:27 -0600

    Res fix

--- moabdb/__init__.py
+++ moabdb/__init__.py
@@ -1,5 +1,5 @@
 """MoabDB"""
-__version__ = "0.1.10"
+__version__ = "0.1.11"
 
 from .lib import *
 from .globals import *
--- moabdb/lib.py
+++ moabdb/lib.py
@@ -46,11 +46,7 @@ def _send_request(request: protocol_pb2.Request) -> protocol_pb2.Response:
         'x-req': b64encode(s)
     }
     res = requests.get(globals._dx_url + 'request/v1/', headers=headers)
-    if res.code == 200:
-        res = protocol_pb2.Response().FromString(b64decode(res.text))
-        return res
-    else:
-        print("Jackson will fix this server error")
+    res = protocol_pb2.Response().FromString(b64decode(res.text))
 
 def _server_req(ticker, start, end, datatype):
     # Request data from moabdb server
