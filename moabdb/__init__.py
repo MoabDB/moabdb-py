9a74af1c7a09f745f2f99568aef388ad624f1ae7
Brian Roseman <brianroseman@gmail.com>
2022-12-07 10:22:25 -0600

    Timewindow fix (#2)
    * Bumped to 0.1.1
    
    * Fixed time window functions

--- moabdb/__init__.py
+++ moabdb/__init__.py
@@ -1,6 +1,7 @@
 """MoabDB"""
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
 from .lib import *
 from .globals import *
 from .protocol_pb2 import *
+from .timewindows import *
--- moabdb/timewindows.py
+++ moabdb/timewindows.py
@@ -1,75 +1,60 @@
 import re
 import requests
+from pandas import DateOffset
 from datetime import datetime
 
-def convert_seconds(date_string):
-    # Get UTC time as integer
-    utc_time = datetime.strptime(date_string, "%Y-%m-%d")
-    tm = (utc_time - datetime(1970, 1, 1)).total_seconds()
+def to_unix_epoch(date_string):
+    # Get uniq epoch time as integer
+    unix_epoch = datetime.strptime(date_string, "%Y-%m-%d")
+    tm = (unix_epoch - datetime(1970, 1, 1)).total_seconds()
     return int(tm)   
 
-def convert_seconds_w_freq(sample_len, base_time):
+def to_unix_w_freq(sample_len, base_time, base_type):
     """
         Provide time frequency and base time, 
             returns offset time.
         Enter negative end_tm as base_time to find start date.
         Enter positive beg_tm as base_time to find end date.
     """
-    tm_freq = int(re.findall(r'(\d+)(\w+?)', sample_len)[0][0])
-    tm_unit = str.upper(re.findall(r'(\d+)(\w+?)', sample_len)[0][1])    
-    if tm_unit == 'D':
-        new_time = base_time + (tm_freq * 86400)
-    elif tm_unit == 'W':
-        new_time = base_time + (tm_freq * 604800)                
-    elif tm_unit == 'M':
-        new_time = base_time + (tm_freq * 1900800)                
-    elif tm_unit == 'Y':
-        new_time = base_time + (tm_freq * 21772800)
+    tm_freq, tm_unit = re.findall(r'(\d+)(\w+?)', sample_len)[0]
+    tm_freq = int(tm_freq) *(-1) if base_type == 'End' else int(tm_freq)
+
+    base_timestamp = pd.Timestamp(base_time, unit='s')
+
+    if str.upper(tm_unit) == 'D':
+        new_time = base_timestamp + DateOffset(days=tm_freq)
+    elif str.upper(tm_unit) == 'W':
+        new_time = base_timestamp + DateOffset(weeks=tm_freq)
+    elif str.upper(tm_unit) == 'M':
+        new_time = base_timestamp + DateOffset(months=tm_freq)
+    elif str.upper(tm_unit) == 'Y':
+        new_time = base_timestamp + DateOffset(years=tm_freq)
     else:
         print("Unknown time unit, accepts: D, W, M, Y")
-    if base_time < 0:
-        new_time *= -1
-    new_time_secs = convert_seconds(new_time)
-    return(new_time_secs)
+    return(new_time.timestamp())
 
+def get_sample_dates(sample_len, start_dt, end_dt):
+    # User provided sample length ...
 
-def get_sample_dts(sample_len, start_dt, end_dt):
-    # User wants a sample window of a specific length and ending today
-    if (sample_len is not None) & (end_dt is None) & (start_dt is None):
+    # ... but didn't provide anything else --> get recent sample
+    if (end_dt is None) & (start_dt is None):
         today = datetime.now().strftime("%Y-%m-%d")
-        end = convert_seconds(today)
-        start = get_seconds_w_freq(sample_len, -end)
+        end = to_unix_epoch(today)
+        start = to_unix_w_freq(sample_len, end, 'End')
 
-    # User wants a sample window of a specific length and starting on a specific date
-    else if (sample_len is not None) & (end_dt is None) & (start_dt is not None):
-        start = convert_seconds(start_dt)   
-        end = get_seconds_w_freq(sample_len, start)
+    # ... and provided start date --> find end date using sample length
+    elif (end_dt is None) & (start_dt is not None):
+        start = to_unix_epoch(start_dt)   
+        end = to_unix_w_freq(sample_len, start, 'Start')
 
-    # User wants a sample window of a specific length and ending on a specific date
-    else if (sample_len is not None) & (end_dt is not None) & (start_dt is None):
-        end = convert_seconds(end_dt)   
-        start = get_seconds_w_freq(sample_len, -end)
+    # ... and provided end date --> find start date using sample length
+    elif (end_dt is not None) & (start_dt is None):
+        end = to_unix_epoch(end_dt)   
+        start = to_unix_w_freq(sample_len, end, 'End')
 
-    else if (sample_len is not None) & (end_dt is not None) & (start_dt is not None):
-        print("Sample length, end date, and start date given ... ignoring sample length")
-        end = convert_seconds(end_dt)
-        start = convert_seconds(start_dt)
-
-    # User wants a sample window between two specific dates
-    else if (sample_len is None) & (end_dt is not None) & (start_dt is not None):
-        end = convert_seconds(end_dt)
-        start = convert_seconds(start_dt)    
-
-    # User didn't provide start date
-    else if (sample_len is None) & (end_dt is not None) & (start_dt is None):
-        sample_len = '30d'
-        end = convert_seconds(end_dt)   
-        start = get_seconds_w_freq(sample_len, -end)
-
-    # User didn't provide end date
-    else if (sample_len is None) & (end_dt is None) & (start_dt is not None):
-        sample_len = '30d'
-        start = convert_seconds(start_dt)   
-        end = get_seconds_w_freq(sample_len, start)
+    # ... and provided start and end date --> ignore sample length
+    elif (end_dt is not None) & (start_dt is not None):
+        end = to_unix_epoch(end_dt)
+        start = to_unix_epoch(start_dt)
 
     return(start, end)
