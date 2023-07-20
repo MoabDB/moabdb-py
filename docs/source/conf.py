e7d7ecf7f7b14c33cd494e3ce6c8089ade109bf8
Brian Roseman <brianroseman@gmail.com>
2023-07-20 18:46:57 -0500

    Get3

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,8 +9,6 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('../../'))
 
-import get_equity2
-
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
