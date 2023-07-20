01a42072123792915143182f9898556211d1d4c2
Brian Roseman <brianroseman@gmail.com>
2023-07-20 10:09:07 -0500

    Docs update

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -7,12 +7,14 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('../../'))
 
+
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = 'MoabDB'
 copyright = '2022, MoabDB'
 author = 'MoabDB'
+
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
