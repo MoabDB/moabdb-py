2ce1556a2384ea8070817ee2718b95c7b57c0a90
Brian Roseman <brianroseman@gmail.com>
2023-07-24 08:41:05 -0500

    equity doc change

--- docs/source/api/get_equity.rst
+++ docs/source/api/get_equity.rst
@@ -1,8 +1,52 @@
-Get Equity Data
-###############
+get_equity
+##########
 
 .. automodule:: moabdb
    :members: get_equity
+
+
+
+Examples
+--------
+
+**Import the library**
+
+>>> import moabdb as mdb
+
+
+Request the last year of ``AAPL`` daily data.
+
+>>> df = mdb.get_equity("AAPL", "1y")
+
+
+Request the most recent year of daily equity data for multiple stocks.
+
+>>> df = mdb.get_equity(["AMZN", "MSFT", "TSLA"], "1y")
+
+
+Request a specific month of daily data.
+
+>>> df = mdb.get_equity("AMZN", start="2022-04-01", sample="1m")
+
+
+Request daily data between two specific dates.
+
+>>> df = mdb.get_equity("AMZN", start="2022-04-01", end="2022-10-01")
+
+
+**Login for intraday data requests**
+
+>>> mdb.login("your_email@example.com", "moabdb_api_key")
+
+
+Request the most recent month of intraday data.
+
+>>> df = mdb.get_equity("TSLA", "1m", intraday=True)
+
+
+Request intraday data between two specific dates:**
+
+>>> df = mdb.get_equity("TSLA", start="2020-01-01", end="2020-06-01", intraday=True)
 
 
 .. `get_equity` function
--- moabdb/core.py
+++ moabdb/core.py
@@ -155,49 +155,6 @@ def get_equity(tickers: Union[str, list],
           with multi-index columns, with the first level being the ticker symbol.
 
 
-    Examples
-    --------
-
-    **Import the library**
-
-    >>> import moabdb as mdb
-
-
-    Request the last year of ``AAPL`` daily data.
-
-    >>> df = mdb.get_equity("AAPL", "1y")
-
-
-    Request the most recent year of daily equity data for multiple stocks.
-
-    >>> df = mdb.get_equity(["AMZN", "MSFT", "TSLA"], "1y")
-
-
-    Request a specific month of daily data.
-
-    >>> df = mdb.get_equity("AMZN", start="2022-04-01", sample="1m")
-
-
-    Request daily data between two specific dates.
-
-    >>> df = mdb.get_equity("AMZN", start="2022-04-01", end="2022-10-01")
-
-
-    **Login for intraday data requests**
-
-    >>> mdb.login("your_email@example.com", "moabdb_api_key")
-
-
-    Request the most recent month of intraday data.
-
-    >>> df = mdb.get_equity("TSLA", "1m", intraday=True)
-
-
-    Request intraday data between two specific dates:**
-
-    >>> df = mdb.get_equity("TSLA", start="2020-01-01", end="2020-06-01", intraday=True)
-
-
     Raises
     ------
     errors.MoabResponseError:
