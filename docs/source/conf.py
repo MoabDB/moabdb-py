fc4b57b07b84b218dedaa429c3fbc4d4b0a45226
Brian Roseman <brianroseman@gmail.com>
2023-08-22 12:32:26 -0500

    revert sphinx

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -50,8 +50,8 @@ release = __version__
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-#html_theme = "sphinx_book_theme"
-html_theme = "pydata_sphinx_theme"
+html_theme = "sphinx_book_theme"
+#html_theme = "pydata_sphinx_theme"
 html_title = f'<strong>{project}</strong> <i>{release}</i>'
 
 html_baseurl = 'https://docs.moabdb.com/'
